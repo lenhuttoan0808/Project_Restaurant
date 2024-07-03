b82f5ead2426b938fefa6693d2dcefaddad988f1
lenhuttoan0808 <lenhuttoan0808@gmail.com>
2024-07-03 17:54:53 +0700

    L1
    Html not cshtml

---
+++ Project_PRN221/PRN221_Admin/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PRN221_Admin
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private ServiceProvider serviceProvider;
+        public App()
+        {
+            //Config for DependencyInjection (01)
+            ServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+        private void ConfigureServices(ServiceCollection services)
+        {
+           // services.AddSingleton(typeof(IBookRepository), typeof(BookRepository));
+            services.AddSingleton<MainWindow>();
+        }
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            var windowBookMianagenent = serviceProvider.GetService<MainWindow>();
+            windowBookMianagenent.Show();
+        }
+    }
+}
